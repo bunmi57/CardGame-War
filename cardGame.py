d36d3cfd218dc25e433faff18dd1cff6064b99b4
Bunmi Akande <bunmiakande57@gmail.com>
2024-12-23 07:44:52 +0100

    Replaced Pop with Remove Moved "availablePosition_player1" and "availablePosition_player2" to global variables
    :wq

--- cardGame.py
+++ cardGame.py
@@ -17,11 +17,6 @@ Game continues untill a player has all the cards
 '''
 
 #Functions
-#Deck of 13 cards
-cardDeck1 = ['King'* 4, 'Queen'* 4, 'Jack'* 4, 
-            '10'* 4, '9'* 4, '8'* 4, '7'* 4,
-            '6'* 4, '5'* 4, '4'* 4, '3'* 4, '2'* 4, '1'* 4]
-#print(len(cardDeck)) # Question: why is this 13 and not 52?
 #*********************************************************************************************************************************
 #Test variables
 testDeck = ['King', 'Queen', 'Jack', 
@@ -56,8 +51,12 @@ def cardDeck():
     completeDeck = []
     incompleteDeck = ['King', 'Queen', 'Jack', 
                       '10', '9', '8', '7',
-                      '6', '5', '4', '3', '2', '1']
-    for i in range(4):
+                      '6', '5', '4', '3', '2']
+    #change back to this
+    # incompleteDeck = ['King', 'Queen', 'Jack', 
+    #                   '10', '9', '8', '7',
+    #                   '6', '5', '4', '3', '2', '1']                     
+    for i in range(1): #change back to 4
         for suite in incompleteDeck:
             completeDeck.append(suite)
    # print(f'Length:{len(completeDeck)}') #52
@@ -67,10 +66,13 @@ def cardDeck():
 #*********************************************************************************************************************************
 #Function to define Card Ranking
 def cardRanking(card):
+    #Change back to this
+    # cardDeckRanking = {'King':1000, 'Queen':900, 'Jack':800, 
+    #                    '10':700, '9':600, '8':500,'7':400, 
+    #                    '6':300, '5':200, '4':100, '3':90, '2':80, '1':70}
     cardDeckRanking = {'King':1000, 'Queen':900, 'Jack':800, 
                        '10':700, '9':600, '8':500,'7':400, 
-                       '6':300, '5':200, '4':100, '3':90, '2':80, '1':70}
-    
+                       '6':300, '5':200, '4':100, '3':90, '2':80}   
     return cardDeckRanking[card]
 #print(cardRanking('2')) #returns 80
 
@@ -90,10 +92,10 @@ def splitDeck(shuffledDeck):
     i = 0
 
     for card in shuffledDeck:
-        if i < 26:
+        if i < 6: #change back to 26
              player1_splitDeck.append(card)
 #             i += 1
-        if i > 25:
+        if i > 5: #change back to 25
             player2_splitDeck.append(card)
         i += 1
     
@@ -103,35 +105,41 @@ def splitDeck(shuffledDeck):
     #     player2_splitDeck.append(j)
     # return player2_splitDeck
 #print(splitDeck(testDeck))
+availablePosition_player1 = [0,1,2,3,4,5]
+availablePosition_player2 = [0,1,2,3,4,5]
 #*********************************************************************************************************************************
 #Function for player to grab a card from the deck
-def grabCard(player,player1_splitDeck,player2_splitDeck):
+def grabCard(player,player1_splitDeck,player2_splitDeck,availablePosition_player1,availablePosition_player2):
     if player == 'player1':
         position_player1 = 'test'
         selectedPosition_player1 = []
-        availablePosition_player1 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+        #change back
+        #availablePosition_player1 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+        #availablePosition_player1 = [0,1,2,3,4,5]
 
         while position_player1 not in availablePosition_player1: #ask player for input if position is not in the list of selected positions
-            print(f'Player 1 Available positions: {availablePosition_player1}')
-            position_player1 = int(input("Select a position: "))
+            #print(f'Player 1 Available positions: {availablePosition_player1}')
+            position_player1 = int(input("Player 1 select a position: "))
         selectedPosition_player1.append(position_player1)
-        availablePosition_player1.pop(position_player1) #remove selected position from the list of available position 
+        availablePosition_player1.remove(position_player1) #remove selected position from the list of available position 
         playedCard_player1 = player1_splitDeck[position_player1]
-        print(f'Updated Available position: {availablePosition_player1}')
+        print(f'Player 1 Updated Available position: {availablePosition_player1}')
         return playedCard_player1
         
     elif player == 'player2':
         position_player2 = 'test'
         selectedPosition_player2 = []
-        availablePosition_player2 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+        #change back
+       # availablePosition_player2 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+        #availablePosition_player2 = [0,1,2,3,4,5]
 
         while position_player2 not in availablePosition_player2: #ask player for input if position is not in the list of selected positions
-            print(f'Player 2 Available positions: {availablePosition_player2}')
-            position_player2 = int(input("Select a position: "))
+            #print(f'Player 2 Available positions: {availablePosition_player2}')
+            position_player2 = int(input("Player 2 select a position: "))
         selectedPosition_player2.append(position_player2)
-        availablePosition_player2.pop(position_player2) #remove selected position from the list of available position 
+        availablePosition_player2.remove(position_player2) #remove selected position from the list of available position 
         playedCard_player2 = player2_splitDeck[position_player2]
-        print(f'Updated Available position: {availablePosition_player2}')
+        print(f'Player 2 Updated Available position: {availablePosition_player2}')
         return playedCard_player2   
 #print(grabCard('player2',testDeck,testDeck))
 #*********************************************************************************************************************************
@@ -151,8 +159,8 @@ battlecards = []
 #Function to take 4 cards when  a 'battle' is declared
 def battle(player1_splitDeck,player2_splitDeck,battlecards):
     #battlecards = []
-    battleCard_player1 = grabCard('player1',player1_splitDeck,player2_splitDeck)
-    battleCard_player2 = grabCard('player2',player1_splitDeck,player2_splitDeck)
+    battleCard_player1 = grabCard('player1',player1_splitDeck,player2_splitDeck,availablePosition_player1,availablePosition_player2)
+    battleCard_player2 = grabCard('player2',player1_splitDeck,player2_splitDec,availablePosition_player1,availablePosition_player2)
     print(f'Player 1 Battle Card: {battleCard_player1}')
     print(f'Player 2 Battle Card: {battleCard_player2}')
  #   battleWinner = compareCard(battleCard_player1, battleCard_player2)
@@ -201,34 +209,48 @@ def roundWinner(play,playedcard_player1,playedcard_player2,player1_splitDeck,pla
   
     return wonDeck_player1, wonDeck_player2 #remember this returns a tuple
 
-#print(roundWinner('play1','King','King',test_player1_splitDeck,test_player2_splitDeck))
-print(roundWinner('battle','King','King',test_player1_splitDeck,test_player2_splitDeck,battlecards))
+
+#print(roundWinner('battle','King','King',test_player1_splitDeck,test_player2_splitDeck,battlecards))
 #if there are multiple battle all the battle cards are not stored
 # 0 4 1 5
 #*********************************************************************************************************************************
 #Function to determine the overall winner
 def overallWinner(wonDeck_player1, wonDeck_player2):
-    if len(wonDeck_player1) == 52:
+    if len(wonDeck_player1) == 12: #change back to 52
         print("Player 1 wins")
         return True
-    elif len(wonDeck_player2) == 52:
+    elif len(wonDeck_player2) == 12: #change back to 52
         print("Player 2 wins")
         return True
     else:
-        pass #do nothing
+        return False #do nothing
 
 #print(overallWinner(testDeck,test_player2_splitDeck))
 #*********************************************************************************************************************************   
 #Game Logic
-# completeDeck = cardDeck()   #create a deck of 52 cards
-# print(f'52 Card Deck: {completeDeck}')
-# shuffledDeck = shuffleDeck(completeDeck)   #Shuffle the deck of 52 cards, returns shuffled deck
-# print(f'Shuffled Deck: {shuffledDeck}')
-# player1_splitDeck,player2_splitDeck = splitDeck(shuffledDeck)
-# print(f'Player 1 Split Deck: {player1_splitDeck}')
-# print(f'Player 2 Split Deck: {player2_splitDeck}')
-# print(f'Player 1 Split Deck Length: {len(player1_splitDeck)}')
-# print(f'Player 2 Split Deck Length: {len(player2_splitDeck)}')
+completeDeck = cardDeck()   #create a deck of 52 cards
+print(f'52 Card Deck: {completeDeck}')
+shuffledDeck = shuffleDeck(completeDeck)   #Shuffle the deck of 52 cards, returns shuffled deck
+print(f'Shuffled Deck: {shuffledDeck}')
+player1_splitDeck,player2_splitDeck = splitDeck(shuffledDeck)
+print(f'Player 1 Split Deck: {player1_splitDeck}')
+print(f'Player 2 Split Deck: {player2_splitDeck}')
+print(f'Player 1 Split Deck Length: {len(player1_splitDeck)}')
+print(f'Player 2 Split Deck Length: {len(player2_splitDeck)}')
+winnerFound = False
+while winnerFound == False:
+    #Players 1 and 2 grab a card each 
+    playedCard_player1 = grabCard('player1',player1_splitDeck,player2_splitDeck,availablePosition_player1,availablePosition_player2)
+    playedCard_player2 = grabCard('player2',player1_splitDeck,player2_splitDeck,availablePosition_player1,availablePosition_player2)
+    #Compare card ranking
+    play = compareCard(playedCard_player1, playedCard_player2)
+    #Determine the winner of the round being played
+    wonDeck_player1,wonDeck_player2 = roundWinner(play,playedCard_player1,playedCard_player2,player1_splitDeck,player2_splitDeck,battlecards)
+    #Check if someone has won the entire game
+    result = overallWinner(wonDeck_player1, wonDeck_player2) 
+    winnerFound = result
+
+
 
 #*********************************************************************************************************************************
 '''
